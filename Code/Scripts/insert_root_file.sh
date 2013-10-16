ca95a5e77b108d9f4c000b1628b2641c7411eabc
KarinaBunyik <karinabunyik@Karinas-MacBook-Air.local>
2013-10-16 17:25:43 +0200

    Paths made generic
    Script made more generic by having paths and filenames as parameters

--- Code/Scripts/insert_root_file.sh
+++ Code/Scripts/insert_root_file.sh
@@ -1,2 +1,3 @@
-cat /Users/karinabunyik/BTSync/Twitter_hidden_topics/Code/Data/root_opening.xml $1 /Users/karinabunyik/BTSync/Twitter_hidden_topics/Code/Data/root_closing.xml > temp.xml
+#!/bin/sh
+cat $2 $1 $3 > temp.xml
 mv temp.xml $1
