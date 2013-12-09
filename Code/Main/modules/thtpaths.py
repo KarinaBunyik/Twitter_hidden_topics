4c12bf86f217ecb8b92ac6006ad76e605e4b474d
KarinaBunyik <karinabunyik@dhcp-189205.eduroam.chalmers.se>
2013-12-09 16:18:46 +0100

    Internal data folder moved
    Internal data folder moved outside of synching.

--- Code/Main/modules/thtpaths.py
+++ Code/Main/modules/thtpaths.py
@@ -4,6 +4,6 @@
 output_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Output/"
 script_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Code/Scripts/"
 data_path = "/Users/karinabunyik/BTSync/Data/"
-internal_data_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Internal_data/"
+internal_data_path = "/Users/karinabunyik/BTSync/Data/Internal/"
 
 # End of thtpaths.py
