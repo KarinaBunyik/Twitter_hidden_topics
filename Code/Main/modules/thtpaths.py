38a832aab9f665a65250d5d75201e16b326fd2db
karinabunyik <kbunyik@gmail.com>
2014-07-03 12:00:24 +0200

    Internal data path change
    The internal data path was changed to an external hard drive.

--- Code/Main/modules/thtpaths.py
+++ Code/Main/modules/thtpaths.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 # Filename: thtpaths.py
 
-output_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Output/"
-script_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Code/Scripts/"
-data_path = "/Users/karinabunyik/BTSync/Data/"
-internal_path = "/Users/karinabunyik/BTSync/Data/Internal/"
-input_path = "/Users/karinabunyik/BTSync/Data/Input/"
+#output_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Output/"
+#script_path = "/Users/karinabunyik/BTSync/Twitter_hidden_topics/Code/Scripts/"
+#data_path = "/Users/karinabunyik/Data/"
+#internal_path = "/Users/karinabunyik/Data/Internal/"
+#input_path = "/Users/karinabunyik/Data/Input/"
+output_path = "/Volumes/Data/Twitter_pldebatt_project/Twitter_hidden_topics/Output/"
+script_path = "/Volumes/Data/Twitter_pldebatt_project/Twitter_hidden_topics/Code/Scripts/"
+data_path = "/Volumes/Data/Twitter_pldebatt_project/Data/"
+internal_path = "/Volumes/Data/Twitter_pldebatt_project/Data/Internal/"
+input_path = "/Volumes/Data/Twitter_pldebatt_project/Data/Input/"
 
 # End of thtpaths.py
