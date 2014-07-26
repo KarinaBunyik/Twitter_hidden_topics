8de97ef91259d4b436cf9087968ce8c41e17987a
karinabunyik <kbunyik@gmail.com>
2014-07-26 11:25:37 +0200

    node scripts added
    tagging and database reformating node scripts

---
+++ node/tagging/app.js
@@ -0,0 +1,150 @@
+var async = require('async');
+var fs = require('fs');
+var _ = require('underscore');
+var MongoClient = require('mongodb').MongoClient;
+
+var connectionString = 'mongodb://localhost:27017/tweets_by_users';
+var collectionName = 'twitter-pldebatt-140504';
+var filePath = '/Volumes/Data/Twitter_pldebatt_project/Data/Internal/tfidf/representative_words/antirasism.txt';
+var stoplistPath = '/Volumes/Data/Twitter_pldebatt_project/Data/Internal/tfidf/representative_words/swedish_stoplist.txt';
+var collection;
+var words;
+var stopwords;
+
+function isArray(a) {
+    return (!!a) && (a.constructor === Array);
+}
+
+function intersect(a, b) {
+    var t;
+    if (b.length > a.length) {
+        t = b;
+        b = a;
+        a = t;
+    } // indexOf to loop over shorter
+    return a.filter(function (e) {
+        if (b.indexOf(e) !== -1) {
+            return true;
+        }
+    });
+}
+
+function isInLemma(lemma){
+    "use strict";
+    var lemma_list = lemma.toString().split("|");
+    //console.log(lemma_list);
+    if (intersect(lemma_list, words).length > 0){
+        return true;
+    }
+    return false;
+}
+
+function loopExtractTweets(callback) {
+    var tweet_number = 0;
+    var taggedTweets = [];
+    var documentNumber = 0;
+    var documentsCount = 0;
+
+    collection.find({}, function (err, docs) {
+
+        if (err) throw err;
+
+        // Count the number of documents
+        docs.count(function(err, res){
+            "use strict";
+            documentsCount = res;
+            console.log("Found " + documentsCount + " documents");
+
+            function processUser(err, user) {
+
+                if(err) throw err;
+                if(user === null) {
+                    callback(null, null);
+                    return; // All done!
+                }
+
+                documentNumber++;
+
+                var tweetsToUpdate = [];
+
+                if (user.text && isArray(user.text)) {
+
+                    user.text.forEach(function (tweet) {
+
+                        tweet_number++;
+
+                        // Print progress
+                        if(tweet_number % 10000 === 0) {
+                            var progress = documentNumber / documentsCount * 100;
+                            console.log(progress + "% / " + tweet_number + " / " + taggedTweets.length);
+                        }
+
+                        tweet.sentence.forEach(function(sent) {
+                            sent.w.forEach(function (word) {
+                                if (!_.contains(stopwords, word.val) && (_.contains(words, word.val) || (isInLemma(word.lemma.toLowerCase())))) {
+
+                                   // Check if tweet has been taged alrady
+                                   if (_.contains(taggedTweets, tweet.id)){
+                                       // No need to update database, return
+                                       //console.log("Tweet " + tweet.id + " has been tagged already");
+                                   }
+                                   else {
+                                        tweetsToUpdate.push(tweet.id);
+                                        taggedTweets.push(tweet.id);
+                                   }
+                                }
+                            });
+                        });
+                    });
+                }
+
+                async.eachSeries(tweetsToUpdate, function(tweet, cb) {
+
+                    // Update the database
+                    collection.update({'text.id': tweet}, {'$addToSet': {'text.$.tweettags': 'antiracism' }}, function (err, res) {
+
+                        // Call the 'internal' async callback cb() so that async knows
+                        // that database has been completed
+                        cb(err, res);
+                    });
+
+                }, function onFinish(err, res){
+
+                   if(err) throw err;
+                    docs.nextObject(processUser);
+                });
+            }
+
+            docs.nextObject(processUser);
+        });
+    });
+}
+
+// Read file to array
+fs.readFile(stoplistPath, function(err, data1) {
+    "use strict";
+    if(err) throw err;
+    stopwords = data1.toString().split("\n");
+
+    fs.readFile(filePath, function(err, data2) {
+
+        if(err) throw err;
+        words = data2.toString().split("\n");
+        console.log("Successfully read in " + words.length + " words");
+
+        // Connect to database
+        MongoClient.connect(connectionString, function(err, db) {
+
+		    if (err) throw err;
+		    console.log("Connected to Database");
+	  	    collection = db.collection(collectionName);
+
+            loopExtractTweets(function onFinish(err, res){
+                if (err){
+                    throw err;
+                }
+                console.log("Finished " + new Date());
+            });
+	    });
+    });
+});
---
+++ node/tweet_based_db/extract_tweets.js
@@ -0,0 +1,126 @@
+var async = require('async');
+var MongoClient = require('mongodb').MongoClient;
+var collectionSource;
+var collectionDestination;
+//var destinationConnectionString = 'mongodb://db1.wndrk.com,db2.wndrk.com,db3.wndrk.com/twitterElection?replicaSet=rs0';
+//var sourceConnectionString = 'mongodb://localhost:27017/twitterDestination';
+var ConnectionString = 'mongodb://localhost:27017/tweets_by_users';
+
+function transferTweets() {
+      var cursor = collectionSource.find({}, {'limit':50}).toArray(function(err, docs) {
+
+        if (docs.length < 1){
+            console.log('No more tweets in here :(');
+            return;
+        }
+
+        async.eachSeries(docs, function(element, callback) {
+        // if any of the saves produced an error, err would equal that error
+          //console.log(element);
+          collectionDestination.save(element,function(err, res) {
+          	if (err) {
+          		console.log(err);
+          		console.log(element);
+          	}
+            collectionSource.remove(element,callback);
+          });
+          
+        },function onFinish(err, res){
+            if (err){
+                console.log(err);
+            }
+            //console.log('Finished! ' + docs.length);
+            transferTweets();
+        });
+      });
+  }
+
+isArray = function(a) {
+    return (!!a) && (a.constructor === Array);
+};
+
+function loopExtractTweets(){
+  //db.open(function(err, db) {
+    //db.collection('Customers', function(err, collection) {
+        collectionSource.find(function(err, cursor) {
+
+        	if (err) throw err;
+
+            cursor.each(function(err, user) {
+
+            	if (err) throw err;
+
+                if (user.text && isArray(user.text)){
+
+                  	async.each(user.text, function(element, callback) {
+	                  	// OH NO, OH NO!!!!
+	                  	// This is a reference, not a copy
+	                    var elementTemp = element;
+	                    elementTemp._id = element.id;
+	                    elementTemp.username = user.username;
+	                    elementTemp.name = user.name;
+	                    elementTemp.following = user.following;
+	                    elementTemp.followers = user.followers;
+	                    elementTemp.location = user.location;
+	                    elementTemp.username = user.username;
+	                    elementTemp.userid = user.id;
+
+	                    if (user.url){
+	                    	elementTemp.url = user.url;
+	                    }
+	                      
+	                    if (user.description){
+	                        elementTemp.description = user.description;
+	                    }
+	                    
+	                    collectionDestination.save(elementTemp, callback);
+	                  
+	                  	}, function onFinish(err, res){
+	                  	if (err){
+	                    	console.log(err);
+	                    }
+                	});
+                }
+               	else {
+
+					var elementTemp = user.text;
+					elementTemp._id = element.id;
+					elementTemp.username = user.username;
+					elementTemp.name = user.name;
+					elementTemp.following = user.following;
+					elementTemp.followers = user.followers;
+					elementTemp.location = user.location;
+					elementTemp.username = user.username;
+					elementTemp.userid = user.id;
+
+					if (user.url){
+						elementTemp.url = user.url;
+					}
+
+					if (user.description){
+						elementTemp.description = user.description;
+					}
+
+					collectionDestination.save(elementTemp, callback);
+	              }
+                  //console.log('Tweet found');
+            });
+        });
+    //});
+//});
+}
+
+
+MongoClient.connect(ConnectionString, function(err, db) {
+  if (err) throw err;
+  console.log("Connected to Database");
+  collectionSource = db.collection('twitter-pldebatt-140504');
+
+    MongoClient.connect(ConnectionString, function(err, db) {
+      if (err) throw err;
+      console.log("Connected to Database");
+      collectionDestination = db.collection('twitter-pldebatt-131006_by_tweets');
+
+      loopExtractTweets();
+    });
+});
